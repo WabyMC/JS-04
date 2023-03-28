84754c5dc50b65cee3ef4fc8621a983f569136b3
Waby M <gabrielamc.046@gmail.com>
2023-03-27 18:59:09 -0600

    Se termino la parte 1, 2 y 3 del ejercicio #1

---
+++ JS-04 - Arrays and Loops/js/main.js
@@ -0,0 +1,15 @@
+let filanombres = ["Sofía","David","Juan"];
+console.log("Parte 1");
+
+console.log(filanombres);
+filanombres.push("Sara");
+filanombres.push("Agustín");
+filanombres.shift();
+console.log("Parte 2");
+
+console.log(filanombres);
+filanombres.splice(1,0,"Renata");
+filanombres.push("Elena");
+console.log("Parte 3");
+console.log(filanombres);
+
